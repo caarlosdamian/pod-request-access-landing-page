de1cf89bdc394397abe88d06d23e289a47fdc065
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-05-11 10:54:45 -0500

    add mobile desing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss';
 import { Middle } from './sections/middle/Middle';
 import { Top } from './sections/top/Top';
+import Bg from './assets/mobile/image-host.jpg'
 
 function App() {
   return (
     <div className="App">
+      <img src={Bg} alt="background"  style={{position:'absolute',width:'100%'}}/>
       <Top />
       <Middle />
     </div>
--- src/sections/top/Top.tsx
+++ src/sections/top/Top.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Logo from '../../assets/desktop/logo.svg'
 import Background from '../../assets/desktop/image-host.jpg'
 import BackgroundTablet from '../../assets/tablet/image-host.jpg'
-import BackgroundMobile from '../../assets/mobile/image-host.jpg'
 import Dots from '../../assets/desktop/bg-pattern-dots.svg'
 import './Top.scss'
 import { useWindowSize } from '../../hooks/useWindowSize'
@@ -11,9 +10,9 @@ export const Top = () => {
   const windowSize = useWindowSize()
   return (
     <div className='top-container'>
-        <img  className='top-logo' src={Logo} alt="logo" />
-        <img className='top-background-img' src={windowSize.width > 768? Background:windowSize.width>375?BackgroundTablet:BackgroundMobile} alt="background" />
-        <img className='top-dots-img' src={Dots} alt="background" />
+      <img className='top-logo' src={Logo} alt="logo" />
+      <img className={`top-background-img ${windowSize.width >= 375 && 'display-none'}`} src={windowSize.width > 768 ? Background :BackgroundTablet} alt="background" />
+      <img className='top-dots-img' src={Dots} alt="background" />
     </div>
   )
 }
