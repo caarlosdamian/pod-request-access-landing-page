137f4197c1a0731c0186c04c36d07d612edabd6d
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-05-11 09:40:01 -0500

    middle and top section finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Bottom } from './sections/bottom/Bottom';
+import { Middle } from './sections/middle/Middle';
+import { Top } from './sections/top/Top';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Top />
+      <Middle />
+      <Bottom />
     </div>
   );
 }
